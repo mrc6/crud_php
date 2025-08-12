2640f338aa506c463bc76de5756570d30b858242
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-08-12 12:18:44 -0300

    Add files via upload

---
+++ usuario-create.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  </head>
+  <body>
+	  <?php include("navbar.php"); ?>
+	  <div class="container mt-5">
+	    <div class="row">
+	      <div class="col-md-12">
+	        <div class="card">
+	          <div class="card-header">
+	            <h4>Adicionar Usuário
+	              <a href="index.php" class="btn btn-danger float-end">Voltar</a>
+	            </h4>
+	          </div>
+	          <div class="card-body">
+	            <form action="" method="POST">
+					<div class="mb-3">
+					  <label>Nome</label>
+					  <input type="text" name="nome" class="form-control"></input>
+					</div>
+					<div class="mb-3">
+					  <label>E-mail</label>
+					  <input type="text" name="email" class="form-control"></input>
+					</div>
+					<div class="mb-3">
+					  <label>Data de Nascimento</label>
+					  <input type="date" name="data_nascimento" class="form-control"></input>
+					</div>
+					<div class="mb-3">
+					  <label>Senha</label>
+					  <input type="password" name="senha" class="form-control"></input>
+					</div>
+					<div class="mb-3">
+					  <button type="submit" name="create_usuario" class="btn btn-primary">Salvar</button>
+					</div>
+	            </form>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  </body>
+</html>
+
